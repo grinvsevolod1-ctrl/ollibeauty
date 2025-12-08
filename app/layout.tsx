75b78b7f76537b62b42204a77824a4292d5246d1
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-12-08 12:55:33 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -23,7 +22,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: Radonly<{
   children: React.ReactNode
 }>) {
   return (
