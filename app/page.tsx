97cf18ce0d28ca6f6351439309d1bcac3f5f49d9
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-12-08 19:12:56 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ import { Footer } from "@/components/footer"
 export default function Page() {
   return (
     <main className="min-h-screen">
-      <Header />
       <HeroVideo />
       <MarqueeText
         items={[
