4a286ad9b01093ae4b9839a3505531030e0e4283
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-12-09 07:31:46 +0300

    ,исправляю пыль

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
-import Silk from "@/components/Silk"
+import Silk from "@/components/silk"
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/next"
